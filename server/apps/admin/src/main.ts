bf0b57652c7cf73bdb662072b807068dca316292
Pin <763331367@qq.com>
2020-02-23 21:29:54 +0800

    第二次修改

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+        .setTitle('全栈')
+        .setDescription('后台API')
+        .setVersion('1.0')
+        .addTag('cats')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-docs', app, document);
+
+    await app.listen(3000);
+    console.log('http://localhost:3000/api-docs')
 }
 bootstrap();
